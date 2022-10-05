9a74dee1719151054b23ba4098f702ddfff6a33f
N Zafar <n_zafar_007@hotmail.com>
2022-10-05 18:12:12 +0200

    first part of homework done

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,17 +1,31 @@
 import express from 'express';
+import keys from "./sources/keys.js";
+import fetch from 'node-fetch';
+const API_KEY = '24bb21182ada6dcc2c538be1bb4be546';
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (request, response) => {
-  res.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Type', 'text/plain');
   response.send('Hello from backend to frontend!');
 });
 
-server.post('/weather', (req, res) => {
+server.post('/weather', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
   const cityName = req.body.cityName;
-  res.setHeader('Content-Type', 'text/plain')
-  res.send(cityName)
+  const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${API_KEY}`);
+  const data = await resp.json();
+  if (data.cod === String(404)) {
+    res.send(`{
+      weatherText: "No city called '${cityName}' found!"
+    }`)
+  } else {
+    res.send(`{
+      weatherText: "${data.name}: ${data.main.temp}"
+    }`);
+
+  }
 })
 
 const PORT = process.env.PORT || 3000; 
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,3 @@
+const API_KEY = '24bb21182ada6dcc2c538be1bb4be546';
+
+export default API_KEY;
