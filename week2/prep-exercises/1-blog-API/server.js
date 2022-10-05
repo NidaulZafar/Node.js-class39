9fa41051d4dea44526eb502f037343e34f0dbbb0
N Zafar <n_zafar_007@hotmail.com>
2022-10-05 10:59:11 +0200

    prep ex part 2 done

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -20,5 +20,24 @@ app.post('/blogs', (req, res) => {
 
 
 
+// Updating existing posts
+app.put('/posts/:title', (req, res) => {
+  // How to get the title and content from the request?
+  // What if the request does not have a title and/or content?
+  const title = req.body.title;
+  const content = req.body.content;
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end('ok')
+  }
+  else {
+    // Send response with error message
+    res.send('This post does not exist!')
+  }
+})
 
-app.listen(3000)
+
+
+
+
+app.listen(3000, () => console.log('server started'))
