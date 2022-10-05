ae21da488b8a7a3491b0e35b62c7c9101e49efe3
N Zafar <n_zafar_007@hotmail.com>
2022-10-05 10:25:05 +0200

    prep ex part1 done

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,24 @@
-const express = require('express')
+const express = require('express');
+const fs = require("fs");
 const app = express();
  
+app.use(express.json());
 
 // YOUR CODE GOES IN HERE
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!')
 })
- 
+
+// Creating new posts
+app.post('/blogs', (req, res) => {
+  // How to get the title and content from the request??
+  const title = req.body.title;
+  const content = req.body.content;
+  fs.writeFileSync(title, content);
+  res.end('ok')
+})
+
+
+
+
 app.listen(3000)
