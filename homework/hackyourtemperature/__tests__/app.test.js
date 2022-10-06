15954fafb5a69ca3ae3b8135fcb551c5702f0045
N Zafar <n_zafar_007@hotmail.com>
2022-10-06 14:24:49 +0200

    testing part of homework

--- homework/hackyourtemperature/__tests__/app.test.js
+++ homework/hackyourtemperature/__tests__/app.test.js
@@ -3,8 +3,24 @@ import app from "../app.js";
 
 const request = supertest(app);
 
-describe("POST /", () => {
-  it("Quick test", () => {
-    expect(1).toBe(1);
+describe("Test the end point req/res Statuses", () => {
+  it("should return status 200 for Homepage", async () => {
+    const response = await request.get("/")
+    expect(response.status).toEqual(200);
+  });
+  it("should return status 200 if valid cityName provided", async () => {
+    const cityName = {cityName: "safsf"}
+    const response = await request.post("/weather").send('"cityName": "safsf"')
+    expect(response.statusCode).toEqual(200);
+  });
+  it("should return status 404 if invalid cityName provided", async () => {
+    const cityName = {cityName: "safsf"}
+    const response = await request.post("/weather").send('"cityName": "safsf"')
+    expect(response.statusCode).toEqual(200);
+  });
+  it("should return status 404 if no cityName provided", async () => {
+    const cityName = {cityName: "safsf"}
+    const response = await request.post("/weather").send('"cityName": "safsf"')
+    expect(response.statusCode).toEqual(200);
   });
 });
